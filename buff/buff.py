abd92abf02b620d6a667b39a9edf12a77067cea5
Ouyang <48503978+MikoZeroer@users.noreply.github.com>
2020-12-09 12:17:10 +0000

    Update buff.py

--- buff/buff.py
+++ buff/buff.py
@@ -10,25 +10,11 @@ if __name__ == "__main__":
 
     # 表头
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/87.0.4280.67 Safari/537.36 Edg/87.0.664.52 '
+        'User-Agent': ''
     }
 
     # BUFF cookie
-    cookie_str = "Device-Id=yFZJ64QHkCtznv0xgxqY; _ga=GA1.2.1833906180.1599195822; " \
-                 "nts_mail_user=DICD2019@163.com:-1:1; mail_psc_fingerprint=7238c1c3a37dc7de06d91c993da5de4f; " \
-                 "_ntes_nnid=204e0e32fcaf713d9e9d6b19d4ef2b0a,1602601777306; " \
-                 "_ntes_nuid=204e0e32fcaf713d9e9d6b19d4ef2b0a; Locale-Supported=zh-Hans; game=csgo; " \
-                 "_gid=GA1.2.1771289923.1607237138; _gat_gtag_UA_109989484_1=1; " \
-                 "NTES_YD_SESS" \
-                 "=0bkZmUIckOuadyZH4ANyRZ86ZjfaGNDBQJMSPezIaal4O1zGOBYERD2ySM4Nly5wzk_tKUELzk" \
-                 "CE5u9QrH_I9jgiDjRNCyLQkUZ9tXsNTYPXX7wk4yAWo4codnYn1snGp0YGhNX.aRhch1WVBO3m.Q8" \
-                 "BWD52raAlZ2eVqJ3UtNzl26FL9m7TV2ap_aWBEG322aSgoCX4grWpp2KelKJeIrfsbmbvbLVaoWexw" \
-                 "gW39j89k; S_INFO=1607237155|0|3&80##|18581573728; P_INFO=18581573728|1607237155" \
-                 "|1|netease_buff|00&99|null&null&null#jil&220100#10#0|&0|null|18581573728; reme" \
-                 "mber_me=U1093767863|xqcEQWjkFCOASBlQWEG6CLNXn2zOy71r; session=1-lpl_QThXkALNQM6" \
-                 "XQ8_VBNy9Qg1lNoFnfnqR_itRbdwP2046758383; csrf_token=IjE0MzFmZTZkNGJiZDA2YWIyMGFlM" \
-                 "GI0ZjUzODRiMTQxZDYzYmIxMzIi.Eq4PqA.ss5yNERSwpiXMoKigepZCnQtKUI "
+    cookie_str = ""
     cookies = {}
     for line in cookie_str.split(';'):
         key, value = line.split('=', 1)
